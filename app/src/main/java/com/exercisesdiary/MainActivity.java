52a99e57566eb3b93fead854928a6e375d60b5fc
Dmitriy Tonkushin <d.tonkushin@abbyy-ls.com>
2018-09-14 18:57:51 +0500

    Added main layout with top menu

---
+++ app/src/main/java/com/exercisesdiary/MainActivity.java
@@ -0,0 +1,32 @@
+package com.exercisesdiary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.top_menu);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater mMenuInflater = getMenuInflater();
+        mMenuInflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    public void add(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+    }
+}
